735f4e9b8fcbdb67dfc40328fe7154dd5b4be45e
nikolagp <petrovskinikolag@gmail.com>
2021-10-11 19:49:21 +0200

    script.js updated

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+console.log(btnShowModal);
+
+// Select the buttons and add event listener to them
+for (let i = 0; i < btnShowModal.length; i++)
+btnShowModal[i].addEventListener('click', function () {
+  console.log('Btn clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+});
+
+// Close modal function
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+// Add event listener to the closing button
+btnCloseModal.addEventListener('click', closeModal);
+
+// Add event listener to the overlay
+overlay.addEventListener('click', closeModal);
